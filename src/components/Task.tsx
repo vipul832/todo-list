41aa45213b7fdcbeb4d76bf4b65574e88155f290
vipulc8 <vipul.c@simformsolutions.com>
2023-04-07 13:45:51 +0530

    update Task font family and button color

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 type TaskProps = {
   text: string;
+  status: boolean;
 };
 
 export default function Task(props: TaskProps) {
   return (
-    <div className="d-flex justify-content-between my-3">
-      <p className="m-0">{props.text}</p>
-      <input type="radio" />
+    <div className="d-flex justify-content-between my-2">
+      <p
+        className={`m-0 fs-5 ${props.status ? "text-secondary" : "text-dark"}`}
+      >
+        {props.text}
+      </p>
+      <input type="checkbox" checked={props.status} />
     </div>
   );
 }
--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -1,17 +1,44 @@
 import React, { Component } from "react";
 import Task from "./Task";
 
+const todoObj = [
+  {
+    task: "Buy new sweatshirt",
+    status: true,
+  },
+  {
+    task: "Begin promotional phase",
+    status: true,
+  },
+  {
+    task: "Read an article",
+    status: false,
+  },
+  {
+    task: "Try not to fall asleep",
+    status: false,
+  },
+  {
+    task: "Watch 'Sherlock'",
+    status: false,
+  },
+  {
+    task: "Begin QA for the product",
+    status: false,
+  },
+  {
+    task: "Go for a walk",
+    status: false,
+  },
+];
+
 export default class TodoTask extends Component {
   render() {
     return (
       <div className="task-area">
-        <Task text={"Buy new sweatshirt"} />
-        <Task text={"Begin promotional phase"} />
-        <Task text={"Read an article"} />
-        <Task text={"Try not to fall asleep"} />
-        <Task text={"Watch 'Sherlock'"} />
-        <Task text={"Begin QA for the product"} />
-        <Task text={"Go for a walk"} />
+        {todoObj.map((todo) => {
+          return <Task text={todo.task} status={todo.status} />;
+        })}
       </div>
     );
   }
